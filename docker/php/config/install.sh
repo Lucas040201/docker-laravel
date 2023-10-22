61006a061141e00801a6740a9f531de630258e67
Lucas Mendes <lucas.mendes@4linux.com.br>
2023-10-22 16:09:45 -0300

    chore: alter php docker image and install dependecies

--- docker/php/config/install.sh
+++ docker/php/config/install.sh
@@ -1,16 +1,33 @@
 #!/bash/sh
 
-# Instalando o Composer
+separator() {
+  echo '::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::'
+  echo $1
+  echo '::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::'
+  echo ''
+  echo ''
+}
+
+# Installing php extensions
+install_dependencies() {
+    separator 'Installing php extensions'
+    apk update && apk add oniguruma-dev zlib-dev libpng-dev postgresql-dev libzip-dev
+    docker-php-ext-install mbstring exif pcntl bcmath gd pgsql pdo_pgsql zip
+    apk update && apk upgrade
+}
+
+# Installing php composer
 install_composer() {
-    echo 'Baixando arquivos do composer ==============='
+    separator 'Install php composer'
     php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-    echo 'Verificando Instalação ========================='
+    separator 'Verify instalation'
     php -r "if (hash_file('sha384', 'composer-setup.php') === 'e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-    echo 'Executando instalação ========================='
+    separator 'Execute Instalation'
     php composer-setup.php
     php -r "unlink('composer-setup.php');"
-    echo 'Movendo binario do composer para /usr/local/bin/composer ========================='
+    separator 'Move composer bin to /usr/local/bin/composer'
     mv composer.phar /usr/local/bin/composer
 }
 
+install_dependencies
 install_composer
